16dc73ac4973394dc3c27e586e79d669f0e83599
Vlad-Vlaicu <vlad_gabriel.vlaicu@stud.acs.upb.ro>
2024-04-18 23:00:13 +0300

    added home fragments

---
+++ app/src/main/java/com/wb/verbum/activities/fragments/HomeFragmentHome.kt
@@ -0,0 +1,18 @@
+package com.wb.verbum.activities.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.wb.verbum.R
+
+class HomeFragmentHome : Fragment(){
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.home_fragment1, container, false)
+    }
+}
